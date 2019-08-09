90f37e0b27e72127eb53f710212ad47b809e2a3a
onlyyou01 <zhanghuaic@163.com>
2019-08-09 10:50:29 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/reset.css'
+// 1像素边框
+import './assets/styles/border.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // 局部组件App
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// @符号指的是src这个目录
+import Home from '@/pages/home/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
